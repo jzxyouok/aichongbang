86be67f1120c1eb2f8fbe2ef3293e842342f5e81
Picsong <312465087@qq.com>
2019-05-06 09:43:03 +0800

    链接数据库

--- aichongbang_S/app.js
+++ aichongbang_S/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('./dao/database');//引入数据库
+
 var app = express();
 
 // view engine setup
