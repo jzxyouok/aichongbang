79ef66251e9c33df7cc8317918fa7124885b4cd7
zhangduxin <yaya15228831604@gmail.com>
2019-05-08 18:11:44 +0800

    Merge branch 'master' of https://github.com/Picsong/aichongbang
    # Conflicts:
    #	aichongbang/src/views/Info.vue

--- aichongbang_S/dao/shopDao.js
+++ aichongbang_S/dao/shopDao.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { shopModel } = require('./models/shopModel');
 
 //获取商店
@@ -37,35 +36,4 @@ module.exports.auditShop = async function (id) {
 //上传
 module.exports.uploadShop = async (shop) => {
     return await shopModel.create(shop);
-=======
-const { shopModel } = require('./models/shopModel');
-
-//获取商店
-module.exports.getShop = async function ({ currentPage, eachPage }) {
-
-    let count = await shopModel.find({ shopStatus: 0 }).countDocuments();  // 总条数
-    let totalPage = Math.ceil(count / eachPage); // 总页数
-    let shops = await shopModel
-        .find({ shopStatus: 0 })
-        .skip((currentPage - 1) * eachPage)
-        .limit(eachPage - 0).populate('userID')
-    let pageData = {
-        currentPage: currentPage - 0,  // 当前页码
-        eachPage: eachPage - 0,  // 每页显示条数
-        totalPage: totalPage - 0,   // 总页数
-        count: count - 0, // 总条数
-        shops  // 电影数据
-    };
-
-    return pageData;
-}
-
-//审核门店
-module.exports.auditShop = async function (id) {
-    return await shopModel.updateOne(id, { shopStatus: 1 });
-}
-//上传
-module.exports.uploadShop = async (shop) => {
-    return await shopModel.create(shop);
->>>>>>> 61fad2fd2f61ca9fbdf9088ac1d4fa886d6fea7f
 }
